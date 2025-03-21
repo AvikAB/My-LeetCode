1542bb166f9bd0368ffd49720bee5be9f3ebcaae
Avik <119071924+AvikAB@users.noreply.github.com>
2025-03-21 11:29:22 +0600

    Time: 67 ms (95.23%), Space: 53.9 MB (8.66%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>cnt;
+        map<int,int>cnt;
         long long sum = 0, ans = 0;
         cnt[0] = 1;
         for(auto av:nums){
