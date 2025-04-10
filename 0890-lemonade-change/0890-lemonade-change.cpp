7758d11169daa129a787b0dfcb55ae655724fc50
Avik <119071924+AvikAB@users.noreply.github.com>
2025-04-10 07:57:07 +0600

    Time: 3 ms (11.51%), Space: 87.2 MB (72.8%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        
+        int five = 0, ten = 0;
+        bool ok = true;
+        for(int i=0; i<bills.size(); i++){
+            if(bills[i]==5) five++;
+            else if(bills[i]==10){
+                if(five){
+                    five--;
+                    ten++;
+                } else ok = false;
+            } else {   // This is for 20
+                if(five and ten){   // 15$ (10+5) return to customer
+                    five--;
+                    ten--;
+                } else if(five>=3) five -= 3;   // 15$ (5+5+5) return to customer
+                else ok = false;
+            }
+        }
+        if(ok) return true;
+        else return false;
     }
 };
