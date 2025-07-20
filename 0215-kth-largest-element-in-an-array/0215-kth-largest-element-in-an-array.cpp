f647c4815d577f81dcd71fd0052e09e50118e568
Avik <119071924+AvikAB@users.noreply.github.com>
2025-07-20 22:11:50 +0600

    Time: 42 ms (21.45%), Space: 61.4 MB (72.46%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for(auto av:nums){
+            minHeap.push(av);
+            if(minHeap.size()>k) minHeap.pop();  // if the size is exceeds the k then it will remove the smallest ele, cause we want to keep only k-largest ele
+        }
+        return minHeap.top();
+    }
+};
