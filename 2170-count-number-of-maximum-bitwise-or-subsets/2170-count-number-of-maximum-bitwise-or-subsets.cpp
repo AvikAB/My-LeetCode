b42502a032cbb9acfe8b32a2a63a1b8b6892e35a
Avik <119071924+AvikAB@users.noreply.github.com>
2025-07-28 18:01:14 +0600

    Time: 133 ms (14.98%), Space: 313.7 MB (8.33%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int countSubsets(int idx, int currOr, int mxOr, vector<int>&nums){
+    int countSubsets(int idx, int currOr, int mxOr, vector<int>&nums, vector<vector<int>>&mem){
         if(idx==nums.size()){
             if(currOr==mxOr) return 1;
             else return 0;
         }
-        int take = countSubsets(idx+1, currOr | nums[idx], mxOr, nums);   // include nums
-        int notTake = countSubsets(idx+1, currOr, mxOr, nums);  // exclude nums
-        return take + notTake;
+        if(mem[idx][currOr] != -1) return mem[idx][currOr];
+
+        int take = countSubsets(idx+1, currOr | nums[idx], mxOr, nums, mem);   // include nums
+        int notTake = countSubsets(idx+1, currOr, mxOr, nums, mem);  // exclude nums
+        return mem[idx][currOr] = take + notTake;
     }
 
     int countMaxOrSubsets(vector<int>& nums) {
@@ -15,7 +17,9 @@ public:
         for(auto &av:nums){
             mxOr |= av;
         }
+        int n = nums.size();
+        vector<vector<int>>mem(n+1, vector<int>(mxOr+1, -1));  // use memoization
         int currOr = 0;
-        return countSubsets(0, currOr, mxOr, nums);
+        return countSubsets(0, currOr, mxOr, nums, mem);
     }
 };
