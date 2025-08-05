b579d3ce518ce0ff5afd24c7119ee2bcc25f5059
Avik <119071924+AvikAB@users.noreply.github.com>
2025-08-05 21:24:45 +0600

    Time: 0 ms (100%), Space: 11.7 MB (48.66%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int left = 0;
-        int right = nums.size()-1;
-        int i = 0;
-        /* Intuition: for sorting, 0 will be placed in left side and 2 will be placed  in right side. Just code will work for that then automatically 1 will be placed in the middle. */
-        while(i<=right){
-            if(nums[i]==0){
-                swap(nums[i], nums[left]);
-                left++, i++;
-            } else if(nums[i]==2){
-                swap(nums[i], nums[right]);
-                right--;    // after swapping i++ will be not working here because it will check that the current element after swapping is 0, 1 or 2. 
-            } else {  // if the element is 1 then it will be placed here (middle place)
-                i++;
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low], nums[mid]);
+                low++, mid++;
+            } else if(nums[mid]==1) mid++;
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
     }
