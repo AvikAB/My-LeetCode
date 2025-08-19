b241d625fbbdd2b915464e5456c6713cc97fcce6
Avik <119071924+AvikAB@users.noreply.github.com>
2025-08-19 18:23:07 +0600

    Time: 0 ms (100%), Space: 111.3 MB (74.5%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -5,9 +5,17 @@ public:
         long long ans = 0, cnt = 0;   // cnt for consecutive 0's
         for(int i=0; i<n; i++){
             if(nums[i]==0) cnt++;
-            else cnt = 0;
-            ans += cnt;
+            else{
+                ans += (1LL*cnt*(cnt+1))/2;
+                cnt = 0;
+            }
         }
+        if(cnt>0) ans += (1LL*cnt*(cnt+1))/2;  // handle the case if array ends with 0s
         return ans;
     }
 };
+
+
+/*
+Intuition: Count consecutive zeroes and do N*(N+1)/2 and add them all.
+*/
