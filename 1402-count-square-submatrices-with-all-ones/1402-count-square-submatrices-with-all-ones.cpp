232500767fd7ab7a82ac4a14c3774239be882f02
Avik <119071924+AvikAB@users.noreply.github.com>
2025-08-20 17:01:59 +0600

    Update 1402-count-square-submatrices-with-all-ones.cpp

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -20,3 +20,33 @@ public:
         return res;
     }
 };
+
+
+// Recursive Way:
+class Solution {
+public:
+    int H,W;
+    // Recursive Way - (Top-Down)
+    int solve(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& memo){
+        if(i>=H or j>=W) return 0;
+        if(matrix[i][j]==0) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        int right = solve(i, j+1, matrix, memo);
+        int bottom = solve(i+1, j, matrix, memo);
+        int diagonal = solve(i+1, j+1, matrix, memo);
+        return memo[i][j] = 1 + min({right, bottom, diagonal});
+    }
+
+    int countSquares(vector<vector<int>>& matrix) {
+        H = matrix.size();
+        W = matrix[0].size();
+        int res = 0;
+        vector<vector<int>> memo(H+1, vector<int>(W+1, -1));
+        for(int i=0; i<H; i++){  // row
+            for(int j=0; j<W; j++){  // col
+                if(matrix[i][j]==1) res += solve(i, j, matrix, memo);
+            }
+        }
+        return res;
+    }
+};
