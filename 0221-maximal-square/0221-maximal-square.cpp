143769fbfc35e6ac15fa79c006dfbe71e2b14199
Avik <119071924+AvikAB@users.noreply.github.com>
2025-08-20 15:27:53 +0600

    Time: 15 ms (31.68%), Space: 31 MB (59.94%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int H = matrix.size();
-        if(H==0 and matrix[0].empty()) return 0;
+        if(H==0) return 0;
         int W = matrix[0].size();
         vector<vector<int>> dp(H, vector<int>(W));
         int ans = 0;
-        for(int row=0; row<H; row++){
-            for(int col=0; col<W; col++){
+        for(int row=H-1; row>=0; row--){
+            for(int col=W-1; col>=0; col--){
                 if(matrix[row][col]=='1'){
                     dp[row][col] = 1;
-                    if(row>0 and col>0){
-                        dp[row][col] += min({dp[row-1][col], dp[row][col-1], dp[row-1][col-1]});
+                    if(row!=H-1 and col!=W-1){
+                        dp[row][col] += min({dp[row+1][col], dp[row][col+1], dp[row+1][col+1]});
                     }
                     ans = max(ans, dp[row][col]);
                 }
@@ -20,3 +20,8 @@ public:
         return ans*ans;
     }
 };
+
+
+/*
+This is traversal from top-left (0,0) to right (R, C+1), down (R+1, C) and Diagonal (R+1, C+1).
+*/
