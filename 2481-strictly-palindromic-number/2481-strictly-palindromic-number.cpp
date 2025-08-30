fd63ffb87a08f4c6bd89b572bbe4b8e2530a2c30
Avik <119071924+AvikAB@users.noreply.github.com>
2025-08-30 19:06:55 +0600

    Time: 0 ms (100%), Space: 7.8 MB (63.71%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
+
+
+// we have given n>=4 so for any integer if you try to write them in more than 1 base system it will not be palindrome
