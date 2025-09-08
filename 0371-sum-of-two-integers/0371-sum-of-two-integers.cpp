f7a5a855405d2283a59a01ac0aad8bdc8e3a737d
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-08 09:58:48 +0600

    Time: 0 ms (100%), Space: 7.8 MB (32.04%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return (a+b);
+        while(b){
+            unsigned carry = (unsigned)(a&b)<<1;  // carry bits
+            a = a^b;
+            b = carry;
+        }
+        return a;
     }
 };
+
+
+
+
+/*
+The XOR operator adding two numbers without considering the carry. The AND operator followed by a left shift (<<1) gives us the carry bits.
+By repeatedly updating the sum and carry, we eventually reach the final result once the carry becomes zero.
+
+Initialize two numbers a and b. While b (carry) is not zero:
+    Compute the carry as (a & b) << 1.
+    Update a as a ^ b (partial sum without carry).
+    Update b as the carry.
+When carry becomes zero, a holds the result of a+b.
+*/
