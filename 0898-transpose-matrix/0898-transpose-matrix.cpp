fe426fb2f4329b4d44ba3afe7cbad9e672e0f7ed
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-09 13:34:20 +0600

    Update 0898-transpose-matrix.cpp

--- 0898-transpose-matrix/0898-transpose-matrix.cpp
+++ 0898-transpose-matrix/0898-transpose-matrix.cpp
@@ -1,3 +1,5 @@
+// This is using extra space:
+
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
@@ -10,5 +12,19 @@ public:
             }
         }
         return mat;
+    }
+};
+
+
+// This is without any extra space:
+class Solution {
+public:
+    void transpose(vector<vector<int>>& matrix) {
+        int n = matrix.size();  // square matrix n x n
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){  // start from i+1
+                swap(matrix[i][j], matrix[j][i]);  // swap across main diagonal, Swapping matrix[i][i] with itself is unnecessary. Thats why j = i+1
+            }
+        }
     }
 };
