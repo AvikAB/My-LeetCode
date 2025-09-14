58bf0298e5e58da8f6362180b940bf17fef6787a
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-14 12:30:38 +0600

    Update 0567-permutation-in-string.cpp

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,3 +1,43 @@
+//Approach-1 (Brute Force) - TLE
+//T.C : O(n! * m)
+//S.C : O(n)
+class Solution {
+public:
+    int n;
+    bool result;
+    void solve(int idx, string& s1, string& s2) {
+        if (idx == n) {
+            if (s2.find(s1) != string::npos) {
+                result = true;
+            }
+            return;
+        }
+
+        for (int i = idx; i < n; i++) {
+            swap(s1[i], s1[idx]);
+            
+            solve(idx + 1, s1, s2);
+
+            swap(s1[i], s1[idx]);
+
+            // Early exit if a permutation has been found in s2
+            if (result == true) 
+                return;
+        }
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        n = s1.length();
+        result = false;  // Reset result for each call
+
+        solve(0, s1, s2);
+
+        return result;  // Return whether a permutation of s1 is found in s2
+    }
+};
+
+
+// Approach: 2 (Sorting - O(n log n)):
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
