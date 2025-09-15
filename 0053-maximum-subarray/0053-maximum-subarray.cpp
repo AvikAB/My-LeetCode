e9cc46eebf0c43481e72daf9a4f0f339c62135a1
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-15 21:25:15 +0600

    Time: 12 ms (5.17%), Space: 74.5 MB (16.51%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0, mx = INT_MIN;
-        for(int i=0; i<nums.size(); i++){
-            sum += nums[i];
-            mx = max(mx, sum);
-            if(sum<0) sum = 0;
+        int n = nums.size();
+        vector<int> ps(n+1, 0);
+        for(int i=0; i<n; i++){
+            ps[i+1] = ps[i]+nums[i];
         }
-        return mx;
+        int mxSum = INT_MIN;
+        int mnSum = 0;
+        for(int i=1; i<=n; i++){
+            mxSum = max(mxSum, ps[i]-mnSum);
+            mnSum = min(mnSum, ps[i]);
+        }
+        return mxSum;
     }
 };
+
+
+
+// Maximum Subarray Sum using Prefix Sum (O(n)) -> Max Prefix Sum - Min Prefix Sum
