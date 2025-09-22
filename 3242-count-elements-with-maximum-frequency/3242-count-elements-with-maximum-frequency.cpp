159c8662ed81a476569b6e837db37bdc10d3cab8
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-22 08:56:15 +0600

    Update 3242-count-elements-with-maximum-frequency.cpp

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -1,3 +1,5 @@
+// Using Hashmap:
+// Time Comp: O(n+u), where u is the num of unique element, Overall TC: O(n)
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
@@ -12,6 +14,25 @@ public:
         int ans = 0;
         for(auto &p:freq){
             if(p.second == mxfr) ans += p.second;
+        }
+        return ans;
+    }
+};
+
+
+// Without hashmap:
+// Time Comp: O(n+101) ~ O(n)
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        vector<int> freq(101, 0);
+        for(int i=0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        int mxfr = *max_element(freq.begin(), freq.end());
+        int ans = 0;
+        for(auto av:freq){
+            if(av == mxfr) ans += av;
         }
         return ans;
     }
