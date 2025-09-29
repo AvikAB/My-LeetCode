616a49c2c4f6f7d6ff6722f9b5d268853210d0be
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-29 13:06:49 +0600

    Time: 4 ms (27.81%), Space: 11.1 MB (70.7%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
+    int solve(vector<int>&v, int i, int j, vector<vector<int>>&dp){
+        if(i+1==j) return 0;  // base case
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = INT_MAX;
+        for(int k=i+1; k<j; k++){
+            ans = min(ans, v[i]*v[j]*v[k] + solve(v,i,k,dp) + solve(v,k,j,dp));
+        }
+        return dp[i][j] = ans;
+    }
+
     int minScoreTriangulation(vector<int>& values) {
         int n = values.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
-        for(int len=2; len<n; len++){
-            for(int i=0; i<n-len; i++){
-                int j = i+len;
-                dp[i][j] = INT_MAX;
-                for(int k=i+1; k<j; k++){
-                    dp[i][j] = min(dp[i][j], dp[i][k]+dp[k][j]+values[i]*values[j]*values[k]);
-                }
-            }
-        }
-        return dp[0][n-1];
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(values, 0, n-1, dp);
     }
 };
