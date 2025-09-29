2430130d130ec70efbe5aa7db5fd89b4a657c3f8
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-29 13:08:13 +0600

    Update 1111-minimum-score-triangulation-of-polygon.cpp

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -1,3 +1,4 @@
+// Using Memoization:
 class Solution {
 public:
     int solve(vector<int>&v, int i, int j, vector<vector<int>>&dp){
@@ -14,5 +15,26 @@ public:
         int n = values.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
         return solve(values, 0, n-1, dp);
+    }
+};
+
+
+
+// Using DP:
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        int n = values.size();
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        for(int len=2; len<n; len++){
+            for(int i=0; i<n-len; i++){
+                int j = i+len;
+                dp[i][j] = INT_MAX;
+                for(int k=i+1; k<j; k++){
+                    dp[i][j] = min(dp[i][j], dp[i][k]+dp[k][j]+values[i]*values[j]*values[k]);
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 };
