23af89162cbc0ab9c9fc9b5383e4ad32cd0c8a3f
Avik <119071924+AvikAB@users.noreply.github.com>
2025-10-15 19:26:58 +0600

    Time: 87 ms (11.98%), Space: 10.5 MB (87.55%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -3,7 +3,7 @@ public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
         if(n<=2) return n;
-        int mx = 2;   // straing line lies on at least 2 points
+        int mx = 2;   // straight line lies on at least 2 points
         // let, i=(x1,y1) -> (i0, i1); same as for j=(x2,y2)->(j0,j1) & k=(x3,y3)->(k0,k1)
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
