0f5465eafcd96d67d14dd1906ba9d9ed1bb4c37c
Avik <119071924+AvikAB@users.noreply.github.com>
2025-10-15 20:47:31 +0600

    Update 0149-max-points-on-a-line.cpp

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,3 +1,32 @@
+// Bruteforce Approach:
+// T.C = O(n^3), S>C = O(1)
+
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        if(n<=2) return n;
+        int mx = 2;   // straight line lies on at least 2 points
+        // let, i=(x1,y1) -> (i0, i1); same as for j=(x2,y2)->(j0,j1) & k=(x3,y3)->(k0,k1)
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int cnt = 2;
+                for(int k=0; k<n; k++){
+                    if(k!=i and k!=j){
+                        if((points[j][1]-points[i][1])*(points[k][0]-points[i][0])==(points[j][0]-points[i][0])*(points[k][1]-points[i][1])) cnt++;
+                    }
+                }
+                mx = max(mx, cnt);
+            }
+        }
+        return mx;
+    }
+};
+
+
+
+// Optimized Approach:
+// Solving by angle of every points. T.C -> O(n^2), S.C -> O(n)
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
