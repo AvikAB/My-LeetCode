0f7fa784d072a0274650be932e3d4901366641a4
Avik <119071924+AvikAB@users.noreply.github.com>
2025-10-15 20:43:06 +0600

    Time: 35 ms (47.39%), Space: 24.3 MB (16.98%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -3,19 +3,20 @@ public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
         if(n<=2) return n;
-        int mx = 2;   // straight line lies on at least 2 points
-        // let, i=(x1,y1) -> (i0, i1); same as for j=(x2,y2)->(j0,j1) & k=(x3,y3)->(k0,k1)
+        int ans = 0;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int cnt = 2;
-                for(int k=0; k<n; k++){
-                    if(k!=i and k!=j){
-                        if((points[j][1]-points[i][1])*(points[k][0]-points[i][0])==(points[j][0]-points[i][0])*(points[k][1]-points[i][1])) cnt++;
-                    }
-                }
-                mx = max(mx, cnt);
+            unordered_map<double,int>mp;  // {slope of the line, how many points has same slope}
+            for(int j=0; j<n; j++){
+                if(j==i) continue;
+                auto dy = points[j][1]-points[i][1];  // y2-y1
+                auto dx = points[j][0]-points[i][0];  // x2-x1
+                auto theta = atan2(dy, dx);
+                mp[theta]++;
+            }
+            for(auto &av:mp){
+                ans = max(ans, av.second+1);  // av.second+1 for including i point
             }
         }
-        return mx;
+        return ans;
     }
 };
