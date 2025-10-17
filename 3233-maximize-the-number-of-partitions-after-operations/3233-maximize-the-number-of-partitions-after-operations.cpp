2c1da69ffb0716ca2d607dbc06365510dabfc7c1
Avik <119071924+AvikAB@users.noreply.github.com>
2025-10-17 20:17:10 +0600

    Time: 539 ms (68.85%), Space: 157.5 MB (53.28%) - LeetHub

--- 3233-maximize-the-number-of-partitions-after-operations/3233-maximize-the-number-of-partitions-after-operations.cpp
+++ 3233-maximize-the-number-of-partitions-after-operations/3233-maximize-the-number-of-partitions-after-operations.cpp
@@ -30,3 +30,12 @@ public:
         return solve(0, 0ULL, true, s, k) + 1;
     }
 };
+
+
+/*
+    i << 27: Left-shifting i by 27 bits. This is done to occupy the most significant bits with the value of i.
+    uniqueChars << 1: Left-shifting uniqueChars by 1 bit. 
+                This is done to make space for the least significant bit, 
+                which will be used to represent the boolean value of canChange.
+    canChange: This boolean value is ORed with the result of the left-shift operations.
+*/
