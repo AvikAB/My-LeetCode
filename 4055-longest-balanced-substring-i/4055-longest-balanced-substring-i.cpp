efa0e1937127d43fc74f6ea2d9ab7f6076e12cc0
Avik <119071924+AvikAB@users.noreply.github.com>
2025-10-23 10:32:58 +0600

    Time: 245 ms (36.84%), Space: 41.5 MB (32.69%) - LeetHub

---
+++ 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isBalance(unordered_map<char, int>&m){  // check that substr is balanced or not (min==max)
+        int mn = INT_MAX;
+        int mx = 0;
+        for(auto &p:m){
+            mn = min(mn, p.second);
+            mx = max(mx, p.second);
+        }
+        return mn == mx;
+    }
+
+    int longestBalanced(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            unordered_map<char, int>m;
+            for(int j=i; j<n; j++){
+                m[s[j]]++;
+                if(isBalance(m)){  //m[s[j]] -> the current freq of the most recently added char
+                    ans = max(ans, j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+};
