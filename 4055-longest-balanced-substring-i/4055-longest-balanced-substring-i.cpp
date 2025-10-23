c04b3d0b620299d76f87c1f850078d3b4400a915
Avik <119071924+AvikAB@users.noreply.github.com>
2025-10-23 10:40:12 +0600

    Implement longest balanced substring function
    Added a method to check if a substring is balanced and implemented the longest balanced substring function.

--- 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.cpp
@@ -22,3 +22,35 @@ public:
         return ans;
     }
 };
+
+
+// Another Easy:
+// Balanced check if its min == max
+
+class Solution {
+public:
+    bool isBalance(unordered_map<char, int>&m){  // check that substr is balanced or not (min==max)
+        int mn = INT_MAX;
+        int mx = 0;
+        for(auto &p:m){
+            mn = min(mn, p.second);
+            mx = max(mx, p.second);
+        }
+        return mn == mx;
+    }
+
+    int longestBalanced(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            unordered_map<char, int>m;
+            for(int j=i; j<n; j++){
+                m[s[j]]++;
+                if(isBalance(m)){
+                    ans = max(ans, j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+};
