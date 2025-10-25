bb97ae1951442bcb0cdf5049a5fb249b1e51c605
Avik <119071924+AvikAB@users.noreply.github.com>
2025-10-25 12:56:44 +0600

    Time: 0 ms (100%), Space: 7.8 MB (86.15%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int ans = 0;
-        int monday$ = 1;
-        while(n>0){
-            int money = monday$;
-            for(int days=1; days<=min(n,7); days++){
-                ans += money;
-                money++;
-            }
-            n -= 7;
-            monday$++;
+        int terms = n/7;  // weeks
+        int firstEle = 28;  // sum of first week as per condition
+        int lastEle = 28+(terms-1)*7;   // formula of finding nth ele of AP, d=7, firstEle=28
+        int ans = terms*(firstEle+lastEle)/2;   // Sum of Arithmatic Progression (AP)
+
+        // remaing days is (n%7), processing starts from below
+        // its also work when n is <7
+        int startMoney = 1+terms;
+        for(int day=1; day<=(n%7); day++){
+            ans += startMoney;
+            startMoney++;
         }
         return ans;
     }
