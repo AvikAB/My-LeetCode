bd1cda55643cb916f7fa27ed8aed173bed3c5dbf
Avik <119071924+AvikAB@users.noreply.github.com>
2025-11-18 14:10:39 +0600

    Time: 0 ms (100%), Space: 13.2 MB (41.51%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
         int n = bits.size();
-        int i = 0;
-        while(i<n-1){
-            if(bits[i]==1) i+=2;
-            else i++;
+        int cnt1 = 0;
+        for(int i=n-2; i>=0 and bits[i]==1; i--){
+            cnt1++;
         }
-        return (i==n-1);
+        return (cnt1%2==0); 
+        /* from the last pos if we get any 0 at this pos, then we will cnt how many 1 occured in
+        their current left side, if its even times then 0 stays at last pos, if odd then it will
+        make pair and create '10'
+        */ 
     }
 };
