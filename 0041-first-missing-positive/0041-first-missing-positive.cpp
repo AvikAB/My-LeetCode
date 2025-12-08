9d32535fe0b5151dae2e02547bd979a7b6f1060e
Avik <119071924+AvikAB@users.noreply.github.com>
2025-12-08 16:09:55 +0600

    Time: 0 ms (100%), Space: 55.1 MB (31.53%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        // cyclic sort: O(n)
+        int n = nums.size();
+        int i = 0;
+        while(i<n){
+            // check num in the range of [1,n],else given runtime error for any neg ele or 0
+            if(nums[i]>0 and nums[i]<=n){
+                int correctIdx = nums[i]-1;
+                // check not in correct index then swap
+                if(nums[i]!=nums[correctIdx]){
+                    swap(nums[i], nums[correctIdx]);
+                } else i++;
+            } else i++;
+        }
+
+        // check first missing positive num
+        for(int i=0; i<n; i++){
+            if(nums[i] != i+1) return i+1;
+        }
+        // If all numbers 1..n are present then 1st missing num is n+1
+        return n+1;
+    }
+};
