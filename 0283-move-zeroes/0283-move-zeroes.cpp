ae9f101ff550b57b9afff93a8ea10c6aa1ad3a84
Avik <119071924+AvikAB@users.noreply.github.com>
2026-01-13 23:09:29 +0600

    Time: 0 ms (100%), Space: 24 MB (19.22%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        for(int j=0; j<nums.size(); j++){
-            if(nums[j]!=0){
-                swap(nums[j], nums[i]);  // swap curr ele with the ele at idx i
-                i++;
+        int pos = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[pos]);
+                pos++;
             }
         }
     }
 };
+
+// non-zero eles are swap with the pos, do nothing to pos when ele is 0, else increment.
