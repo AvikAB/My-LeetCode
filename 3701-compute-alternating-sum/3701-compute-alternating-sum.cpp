9c8b8b039866a4c8fd042580ac35a92c13fd4b2b
Avik <119071924+AvikAB@users.noreply.github.com>
2026-01-14 12:54:14 +0600

    Time: 0 ms (100%), Space: 32.4 MB (78.41%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int alternatingSum(vector<int>& nums) {
         int evnSum = 0, oddSum = 0;
-        for(int i=0; i<nums.size(); i+=2){
-            evnSum += nums[i];
-        }
-        for(int i=1; i<nums.size(); i+=2){
-            oddSum += nums[i];
+        for(int i=0; i<nums.size(); i++){
+            if(i%2) oddSum += nums[i];
+            else evnSum += nums[i];
         }
         return evnSum-oddSum;
     }
