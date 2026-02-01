f9d4d2e368aa06db37ba2abd1861ad581d926964
Avik <119071924+AvikAB@users.noreply.github.com>
2026-02-01 09:55:42 +0600

    Time: 0 ms (100%), Space: 30.6 MB (48.24%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int mn1 = INT_MAX, mn2 = INT_MAX;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]<mn1){
+                mn2 = mn1;
+                mn1 = nums[i];
+            } else if(nums[i]<mn2){
+                mn2 = nums[i];
+            }
+        }
+        return nums[0]+mn1+mn2;
+    }
+};
+
+
+/*
+First ele of that array is fixed as a subarr, now find min1 and min2. From min1, the new
+subarr is start, From min2, the another new subarr is start.
+*/
