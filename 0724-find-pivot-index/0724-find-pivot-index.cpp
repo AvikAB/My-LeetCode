6dbe857a2ca8cf321f16bfcc2d67c83e95da3327
Avik <119071924+AvikAB@users.noreply.github.com>
2026-02-23 00:31:47 +0600

    Time: 3 ms (20.18%), Space: 37.1 MB (5.4%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        // prefix sum
+        vector<int> pref(n+1, 0);
+        for(int i=0; i<n; i++){
+            pref[i+1] = pref[i] + nums[i];
+        }
+        int totSum = pref[n];
+        // suffix sum
+        vector<int> suff(n+1, 0);
+        for(int i=0; i<n; i++){
+            suff[i] = totSum - pref[i];
+        }
+
+        for(int i=0; i<n; i++){
+            if(pref[i]==suff[i+1]) return i;
+        }
+        return -1;
+    }
+};
