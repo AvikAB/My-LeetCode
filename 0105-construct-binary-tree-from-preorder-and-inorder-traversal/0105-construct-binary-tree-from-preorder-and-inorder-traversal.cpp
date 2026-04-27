4aa636375e73418c25fa964f98af3ecaddb0ddd7
Avik <119071924+AvikAB@users.noreply.github.com>
2026-04-27 13:03:31 +0600

    Add O(n) approach for binary tree construction
    Implemented an O(n) approach for constructing a binary tree from preorder and inorder traversals using a hashmap for index mapping.

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -40,3 +40,30 @@ class Solution {
 };
 
 // O(n^2) Approach
+
+// O(n) Approach using hashmap:
+class Solution {
+    unordered_map<int,int> inorderMap;  // val -> index mapping
+    
+    TreeNode* Tree(vector<int>&preorder, int InSt, int InEnd, int &idx){
+        if(InSt>InEnd) return NULL;
+        
+        TreeNode *root = new TreeNode(preorder[idx]);
+        int pos = inorderMap[preorder[idx]];
+        idx++;
+        
+        root->left = Tree(preorder, InSt, pos-1, idx);
+        root->right = Tree(preorder, pos+1, InEnd, idx);
+        
+        return root;
+    }
+    
+public:
+    TreeNode *buildTree(vector<int>&preorder, vector<int>&inorder) {
+        for(int i=0; i<inorder.size(); i++){
+            inorderMap[inorder[i]] = i;
+        }
+        int preorderIndex = 0;
+        return Tree(preorder, 0, inorder.size()-1, preorderIndex);
+    }
+};
