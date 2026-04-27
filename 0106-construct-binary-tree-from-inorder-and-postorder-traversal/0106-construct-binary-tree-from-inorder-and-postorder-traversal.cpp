c976467d6d9c9a6f0d97c1b55f8fd63ccaa9a127
Avik <119071924+AvikAB@users.noreply.github.com>
2026-04-27 13:17:25 +0600

    Implement linear search for inorder index lookup
    Added a linear search approach for finding the index of a target in the inorder traversal.

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -38,3 +38,34 @@ class Solution {
 };
 
 // O(n) Approach using hashmap
+
+
+// Aprroach 2: O(n^2) for linear search
+class Solution {
+  public:
+    int find(vector<int>inorder, int target, int start, int end){
+        for(int i=start; i<=end; i++){
+            if(inorder[i]==target) return i;
+        }
+        return -1;
+    }
+    
+    TreeNode* Tree(vector<int> &inorder, vector<int> &postorder, int InSt, int InEnd, int idx){
+        if(InSt>InEnd) return NULL;
+        
+        TreeNode* root = new TreeNode(postorder[idx]);
+        int pos = find(inorder, postorder[idx], InSt, InEnd);
+        
+        // Right:
+        root->right = Tree(inorder, postorder, pos+1, InEnd, idx-1);
+        // left:
+        root->left = Tree(inorder, postorder, InSt, pos-1, idx-(InEnd-pos)-1);
+        return root;
+    }
+    
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        // code here
+        int n = inorder.size();
+        return Tree(inorder, postorder, 0, n-1, n-1);
+    }
+};
