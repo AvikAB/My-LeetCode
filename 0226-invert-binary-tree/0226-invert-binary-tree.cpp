28815a49023d98c68d12b6c4922b3779e1a1e40b
Avik <119071924+AvikAB@users.noreply.github.com>
2026-06-07 06:27:04 +0600

    Time: 0 ms (100%), Space: 12.3 MB (98.77%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -11,14 +11,20 @@
  */
 class Solution {
 public:
+    TreeNode* dfs(TreeNode* root){
+        if(root==NULL) return NULL;
+        TreeNode* left = dfs(root->left);
+        TreeNode* right = dfs(root->right);
+
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        return root;
+    }
+
     TreeNode* invertTree(TreeNode* root) {
-        if(root==nullptr) return nullptr;
-        
-        TreeNode* left = invertTree(root->left);
-        TreeNode* right = invertTree(root->right);
-        
-        root->left = right;
-        root->right = left;
+        if(root==NULL) return NULL;
+        dfs(root);
         return root;
     }
 };
