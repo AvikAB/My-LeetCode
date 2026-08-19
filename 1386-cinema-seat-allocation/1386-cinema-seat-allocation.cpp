198dc5495d709a8f75596a83d11ae06556e5eb3f
Avik <119071924+AvikAB@users.noreply.github.com>
2026-08-19 13:57:16 +0600

    Time: 22 ms (73.82%), Space: 60.2 MB (68.07%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int, unordered_set<int>>mp;  // row -> booked seats in row
+        unordered_map<int, int>mp;  // row -> booked seats in row
         for(auto &reserve:reservedSeats){
             int row = reserve[0];
             int seat = reserve[1];
-            mp[row].insert(seat);
+            mp[row] |= (1<<seat);
         }
         int ans = (n-mp.size())*2;  // atmost we can take 2 groups at a time
         for(auto &[row, booked]:mp){
-            auto isAvail = [&](int seat){
-                return booked.find(seat)==booked.end();
-            };
+            int maskA = (1<<2) | (1<<3) | (1<<4) | (1<<5);
+            int maskB = (1<<4) | (1<<5) | (1<<6) | (1<<7);
+            int maskC = (1<<6) | (1<<7) | (1<<8) | (1<<9);
 
-            bool grA = isAvail(2) and isAvail(3) and isAvail(4) and isAvail(5);
-            bool grB = isAvail(4) and isAvail(5) and isAvail(6) and isAvail(7);
-            bool grC = isAvail(6) and isAvail(7) and isAvail(8) and isAvail(9);
+            bool grA = (booked & maskA) == 0;  // all seats are available
+            bool grB = (booked & maskB) == 0;
+            bool grC = (booked & maskC) == 0;
 
             if(grA and grC) ans += 2;  // atmost 2 groups are A & C
             else if(grA or grB or grC) ans++;
