6f555e48593ab8e5490d231173c66f5e90ef333a
Avik <119071924+AvikAB@users.noreply.github.com>
2026-08-26 11:28:42 +0600

    Time: 0 ms (100%), Space: 8.8 MB (64.74%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        // bruteforce
+        // optimized
         int n = s.size();
         string ans = "";
-        for(int i=0; i<n; i++){
-            int cnt1 = 0;
-            for(int j=i; j<n; j++){
-                if(s[j]=='1') cnt1++;
-                if(cnt1==k){
-                    string curr = s.substr(i, j-i+1);
+        int cnt1 = 0;
+        int l = 0, r = 0;
+        while(r<n){
+            if(s[r]=='1') cnt1++;
+            if(cnt1==k){
+                while(l<n and cnt1==k){   // shrink from left for small len
+                    string curr = s.substr(l, r-l+1);
                     if(ans=="" or curr.size()<ans.size()) ans = curr;
-                    else if(curr.size()==ans.size()) ans = min(curr, ans);
+                    else if(ans.size()==curr.size()) ans = min(ans, curr);
+                    if(s[l]=='1') cnt1--;
+                    l++;
                 }
             }
+            r++;
         }
         return ans;
     }
