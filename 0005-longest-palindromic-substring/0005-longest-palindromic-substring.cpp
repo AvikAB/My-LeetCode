e0b1ab8377438dc0978f0d63e635c5714a9f6e2e
Avik <119071924+AvikAB@users.noreply.github.com>
2026-09-18 13:50:00 +0600

    Time: 17 ms (42.48%), Space: 18.5 MB (37.48%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    // Top Down DP
-    int dp[1001][1001];
-    // bool isPalin(string &s, int l, int r){
-    //     while(l<r){
-    //         if(s[l]!=s[r]) return false;
-    //         l++;
-    //         r--;
-    //     }
-    //     return true;
-    // }
-
-    bool solve(string &s, int l, int r){
-        if(l>=r) return 1;
-
-        if(dp[l][r]!=-1) return dp[l][r];
-
-        if(s[l]==s[r]) return dp[l][r] = solve(s, l+1, r-1);
-        
-        return dp[l][r] = false;
-
+    // Two Pointers
+    int n;
+    string expand_from_center(const string &s, int l, int r){
+        // starting from center, if they are equal then expand from both
+        while(l>=0 and r<n and s[l]==s[r]){
+            l--;
+            r++;
+        }
+        return s.substr(l+1, r-l-1);
     }
 
     string longestPalindrome(string s) {
-        int n = s.size();
-        int bestStart = 0, mxLen = 0;
-        memset(dp, -1, sizeof(dp));
+        n = s.size();
+        if(n==0) return "";
+        string mx = s.substr(0,1);
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(solve(s, i, j)){
-                    if(j-i+1>mxLen){
-                        mxLen = j-i+1;
-                        bestStart = i;
-                    }
-                }
-            }
+            string odd = expand_from_center(s, i, i);
+            string even = expand_from_center(s, i, i+1);
+            if(odd.size()>mx.size()) mx = odd;
+            if(even.size()>mx.size()) mx = even;
         }
-    
-        return s.substr(bestStart, mxLen);
+        return mx;
     }
 };
