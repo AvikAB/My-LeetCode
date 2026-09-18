e25127f79a04f9ee3329b0deea89051537f6e0f8
Avik <119071924+AvikAB@users.noreply.github.com>
2026-09-18 13:00:48 +0600

    Time: 172 ms (21.5%), Space: 13.9 MB (40.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
-    // bruteforce
-    bool isPalin(string &s, int l, int r){
-        while(l<r){
-            if(s[l]!=s[r]) return false;
-            l++;
-            r--;
-        }
-        return true;
+    // Top Down DP
+    int dp[1001][1001];
+    // bool isPalin(string &s, int l, int r){
+    //     while(l<r){
+    //         if(s[l]!=s[r]) return false;
+    //         l++;
+    //         r--;
+    //     }
+    //     return true;
+    // }
+
+    bool solve(string &s, int l, int r){
+        if(l>=r) return 1;
+
+        if(dp[l][r]!=-1) return dp[l][r];
+
+        if(s[l]==s[r]) return dp[l][r] = solve(s, l+1, r-1);
+        
+        return dp[l][r] = false;
+
     }
 
     string longestPalindrome(string s) {
         int n = s.size();
-        int bestStart = 0, mxLen = 1;
+        int bestStart = 0, mxLen = 0;
+        memset(dp, -1, sizeof(dp));
         for(int i=0; i<n; i++){
             for(int j=i; j<n; j++){
-                if(isPalin(s, i, j)){
-                    int len = j-i+1;
-                    if(len>mxLen){
-                        mxLen = len;
+                if(solve(s, i, j)){
+                    if(j-i+1>mxLen){
+                        mxLen = j-i+1;
                         bestStart = i;
                     }
                 }
             }
         }
+    
         return s.substr(bestStart, mxLen);
     }
 };
