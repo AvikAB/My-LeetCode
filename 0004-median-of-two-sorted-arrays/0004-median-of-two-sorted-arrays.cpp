a3b0661bb02b4e80680913a396c7011426be410a
Avik <119071924+AvikAB@users.noreply.github.com>
2026-09-20 12:19:54 +0600

    Time: 0 ms (100%), Space: 95 MB (94.6%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
+    // two pointer:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
-        for(auto av:nums1) v.push_back(av);
-        for(auto av:nums2) v.push_back(av);
-        int n = v.size();
-        sort(v.begin(), v.end());
-
-        if(n%2) return v[n/2];
-        else return (v[n/2 - 1] + v[n/2]) / 2.0;
+        int n = nums1.size(), m = nums2.size();
+        int tot = (n+m);
+        int mid1 = (tot-1)/2;   // indexOfmid1
+        int mid2 = tot/2;       // indexOfmid2
+        int i=0, j=0;
+        int cnt = 0;
+        int a=0, b=0;
+        while(cnt<=mid2){
+            int val;
+            if(i<n and (j>=m or nums1[i]<=nums2[j])) val = nums1[i++];
+            else val = nums2[j++];
+            if(cnt==mid1) a = val;
+            if(cnt==mid2) b = val;
+            cnt++;
+        }
+        return (tot%2==0) ? (a+b)/2.0 : b;
     }
 };
 
 
-// O((n1+n2)log(n1+n2)) = not optimal
+// O(n+m/2) ~ O(n+m)
