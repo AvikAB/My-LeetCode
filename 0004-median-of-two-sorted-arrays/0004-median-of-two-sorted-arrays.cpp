ad8f759097b75a354302a1d2af15f422e5638c8b
Avik <119071924+AvikAB@users.noreply.github.com>
2026-09-20 15:36:05 +0600

    Time: 0 ms (100%), Space: 95 MB (79.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
-    // two pointer:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
-        int tot = (n+m);
-        int mid1 = (tot-1)/2;   // indexOfmid1
-        int mid2 = tot/2;       // indexOfmid2
-        int i=0, j=0;
-        int cnt = 0;
-        int a=0, b=0;
-        while(cnt<=mid2){
-            int val;
-            if(i<n and (j>=m or nums1[i]<=nums2[j])) val = nums1[i++];
-            else val = nums2[j++];
-            if(cnt==mid1) a = val;
-            if(cnt==mid2) b = val;
-            cnt++;
+        int m = nums1.size();
+        int n = nums2.size();
+        if(m>n){
+            return findMedianSortedArrays(nums2, nums1);  // always doing bin search in small array
         }
-        return (tot%2==0) ? (a+b)/2.0 : b;
+
+        int l = 0, r = m;
+        while(l<=r){
+            int mid = (l+r)/2;  // ele taken from nums1 into left_half
+            int rem_left = (m+n+1)/2-mid;  // ele taken from nums2 into left_half
+
+            // left_half
+            int l1 = (mid==0) ? INT_MIN : nums1[mid-1];
+            int l2 = (rem_left==0) ? INT_MIN : nums2[rem_left-1];
+
+            // right_half
+            int r1 = (mid==m) ? INT_MAX : nums1[mid];
+            int r2 = (rem_left==n) ? INT_MAX : nums2[rem_left];
+
+            if(l1<=r2 and l2<=r1){
+                if((m+n)%2) return max(l1, l2);
+                else return (max(l1,l2)+min(r1,r2))/2.0;
+            }
+
+            if(l1>r2){  // not sorted
+                r = mid-1;
+            } else l = mid+1;
+        }
+        return -1;
     }
 };
 
 
-// O(n+m/2) ~ O(n+m)
+// O(log(m+n))
+
+/*
+There will be total 2 partition: left_half, right_half
+- left_half's all vals always will be lesser than the right_half.
+[....l1 r1.....]
+[....l2 r2.....]
+- Condition: l1<=r2 and l2<=r1.
+We know l1<=r1 because of its sorted, we also need to check that 1st left_half is less than the 2nd right_half for valid partition to get median.
+
+- mid: how many eles we can take from nums1 into left_half.
+- rem_left: the remaining size of left_half from the nums2.
+
+- For odd size, left_half's largest val will be median.
+- For even size, left_half's max & right_half's min's avg will be median.
+*/
