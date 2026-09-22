3aab72e35f21b3382ccf4fe8da61b36587613d63
Avik <119071924+AvikAB@users.noreply.github.com>
2026-09-22 09:22:36 +0600

    Time: 0 ms (100%), Space: 12.8 MB (17.15%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = max_element(nums.begin(),nums.end())-nums.begin();
-        return n;
+        int n = nums.size();
+        int l = 0, r = n-1;
+        while(l<r){
+            int mid = (l+r)/2;
+            if(nums[mid]>nums[mid+1]){  // mid can be peak, now check from left side
+                r = mid;
+            } else {  // n[mid]<n[mid+1] then mid can't be peak, mid+1 can be peak, so check from right side
+                l = mid+1;
+            }
+        }
+        return l;
     }
 };
